daa36807a4d092580a7ea827d0db589a7cc779a3
Mohamed Hary <mohamedhary195@gmail.com>
2024-12-08 22:53:42 +0300

    add Tow numbers and reserve string

--- Tutorial.py
+++ Tutorial.py
@@ -1,1 +1,13 @@
-print("hhhahsvA")
+# Check Even or Odd
+Num = int(input("inter number : "))
+if Num%2 ==0:
+    print(" the Number is Even ")
+else:
+    print("The number is Odd ")
+
+#.................
+# Reverse a String
+st = input("Inter string: ")
+Rev_st = st[::-1]
+print(Rev_st)
+
